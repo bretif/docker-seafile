753006753eda3e28f75a29dc00e08351b9ed0f6c
Bertrand Rétif <br@phosphore.eu>
2014-10-03 02:00:21 +0200

    Correct log location 	modifié:         seafile.sh 	modifié:         seahub.sh

--- seahub.sh
+++ seahub.sh
@@ -2,7 +2,12 @@
 
 [ "${autostart}" = 'true' -a -x /opt/seafile/seafile-server-latest/seahub.sh ] || exit 0
 
-/opt/seafile/seafile-server-latest/seahub.sh start >>/opt/seafile/log/seafile.log 2>&1
+if [ "${fcgi}" = 'true' ];
+then
+    /opt/seafile/seafile-server-latest/seahub.sh start start-fastcgi 8000 >> /opt/seafile/logs/seafile.log 2>&1
+else
+    /opt/seafile/seafile-server-latest/seahub.sh start >> /opt/seafile/logs/seafile.log 2>&1
+fi
 
 # Script should not exit unless seahub died
 while pgrep -f "manage.py run_gunicorn" 2>&1 >/dev/null; do
