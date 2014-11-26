2fdd982ff3c2fc0bd29836cdbe3353c1ddfbd926
Guilhem Berna <guilhem.berna@gmail.com>
2014-11-26 10:55:26 +0100

    fixed db host set at the wrong place

--- scripts/setup-seafile-mysql.py
+++ scripts/setup-seafile-mysql.py
@@ -1211,13 +1211,15 @@ def main():
     seafile_config.seafile_dir = os.path.join(env_mgr.top_dir, 'seafile-data')
     seafile_config.port = os.environ.get('SEAFILE_PORT')
     seafile_config.fileserver_port = os.environ.get('FILESERVER_PORT')
-    db_config.mysql_host = os.environ.get('MYSQL_HOST')
-    db_config.mysql_port = os.environ.get('MYSQL_PORT')
     
     if os.environ.get('EXISTING_DB') == 'true':
         db_config = ExistingDBConfigurator()
+        db_config.mysql_host = os.environ.get('MYSQL_HOST')
+        db_config.mysql_port = os.environ.get('MYSQL_PORT')
     else:
         db_config = NewDBConfigurator()
+        db_config.mysql_host = os.environ.get('MYSQL_HOST')
+        db_config.mysql_port = os.environ.get('MYSQL_PORT')
         db_config.root_password = os.environ.get('MYSQL_ROOT_PASSWORD')
         db_config.root_conn = db_config.check_mysql_user('root', db_config.root_password)
         
